3d7fed28c3e11faf9e133926a5783b937762edb1
hidekazoo <hidekazoo.crudzoo@gmail.com>
2026-01-21 08:17:21 +0900

    add nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,8 +28,9 @@ vim.opt.rtp:prepend(lazypath)
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2   
 vim.opt.expandtab = true
-
-
+vim.opt.termguicolors = true
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- Setup lazy.nvim
 require("lazy").setup({
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,15 +1,21 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    opts = {},
-    config = function(_, opts)
-      require("tokyonight").setup(opts)
-    end,
-  },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   opts = {
+  --   },
+  --   config = function(_, opts)
+  --     require("tokyonight").setup(opts)
+  --   end,
+  -- },
   { "EdenEast/nightfox.nvim" ,
   lazy = false,
   priority = 1000,
+  opts = {
+    options = {
+      transparent = true,
+    }
+  },
   config = function()
     vim.cmd.colorscheme("nightfox")
   end,
