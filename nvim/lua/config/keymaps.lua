be116c6a4aaeb36375925cb7cb7549662e2bd68e
hidekazoo <hidekazoo.crudzoo@gmail.com>
2026-02-04 22:40:37 +0900

    lazygit

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,18 +1,3 @@
-vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save File" })
-vim.keymap.set('n', '<leader>sw', function()
-  -- 現在のテーマが nightfox なら dayfox に、それ以外なら nightfox に変更
-  if vim.g.colors_name == "nightfox" then
-    vim.cmd("colorscheme dayfox")
-    print("Switched to dayfox")
-  else
-    vim.cmd("colorscheme nightfox")
-    print("Switched to nightfox")
-  end
-end, { desc = "Toggle nightfox/dayfox" })
-vim.keymap.set("n", "gl", function()
-  vim.diagnostic.open_float()
-end, { desc = "Show line diagnostics" })
-vim.keymap.set({ "n", "v" }, "<D-.>", vim.lsp.buf.code_action, { desc = "Code Action" })
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { silent = true })
-vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], { silent = true })
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,23 +1,25 @@
 return {
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   lazy = false,
-  --   opts = {
-  --   },
-  --   config = function(_, opts)
-  --     require("tokyonight").setup(opts)
-  --   end,
-  -- },
-  { "EdenEast/nightfox.nvim" ,
-  lazy = false,
-  priority = 1000,
-  opts = {
-    options = {
-      transparent = true,
-    }
+  -- Nightfox本体の追加
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = false, -- 起動時に読み込む必要があるため
+    priority = 1000, -- 他のプラグインより先に読み込む
+    config = function()
+      -- 必要ならここにNightfox独自のオプションを書けます
+      -- 例: 背景を透明にするなど
+      require("nightfox").setup({
+        options = {
+          transparent = false,
+        },
+      })
+    end,
   },
-  config = function()
-    vim.cmd.colorscheme("nightfox")
-  end,
+
+  -- LazyVim自体の設定でカラースキームを指定
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nightfox",
+    },
   },
 }
