8817ee686ee563866d2ae1ed5204479e15930b13
hidekazoo <hidekazoo.crudzoo@gmail.com>
2026-02-23 22:45:43 +0900

    fix

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,12 +1,14 @@
 return {
-  -- Nightfox本体の追加
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 900,
+  },
   {
     "EdenEast/nightfox.nvim",
-    lazy = false, -- 起動時に読み込む必要があるため
-    priority = 1000, -- 他のプラグインより先に読み込む
+    lazy = false,
+    priority = 1000,
     config = function()
-      -- 必要ならここにNightfox独自のオプションを書けます
-      -- 例: 背景を透明にするなど
       require("nightfox").setup({
         options = {
           transparent = false,
@@ -14,12 +16,11 @@ return {
       })
     end,
   },
-
   -- LazyVim自体の設定でカラースキームを指定
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nightfox",
+      colorscheme = "kanagawa",
     },
   },
 }
